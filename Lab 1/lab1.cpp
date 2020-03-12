1bcb81d1fde47464afd6d6cd179ce5fb7bdea2c0
Andy <iliescuandrew@gmail.com>
2020-03-12 08:29:59 -0500

    cpp file didn't go through the last commit

--- Lab 1/lab1.cpp
+++ Lab 1/lab1.cpp
@@ -9,18 +9,51 @@ int main(int argc, char* argv[]){
 	string filename = argv[1];
 	ifstream filein(filename.c_str());
 	string line;
+    string word;
+    string data[] = {};
+    int numFacets = 0;
+    int x,y,z = 0;
+    int min_x,min_y,min_z = 0;
+    int max_x,max_y,max_z = 0;
+
     while(getline(filein, line)){
-        if(line == "vertex"){
-            cout << line << endl;
+        istringstream ss(line);
+        ss >> word;
+        if(word == "vertex"){
+            ss >> x;
+            ss >> y;
+            ss >> z;
+            if(x > max_x){
+                max_x = x;
+            }
+            if(x < min_x){
+                min_x = x;
+            }
+            if(y > max_y){
+                max_y = y;
+            }
+            if(y < min_y){
+                min_y = y;
+            }
+            if(z > max_z){
+                max_z = z;
+            }
+            if(z < min_z){
+                min_z = z;
+            }
+            //data[numFacets] = line; //SEG FAULTS HERE
+            numFacets++;
         }
     }
-    istringstream ss(line);
-    string word;
-    
-    ss >> word;
-    double number;
-    ss >> number;
-    cout << fixed << setprecision(4) << setw(20) << number << setw(20) << number << endl;
+    numFacets /= 3;
+    int count = 0;
+    // for(int i = 0; i < numFacets; i++){
+    //     printf("Facet: %d       x   y   z", i);
+    //     while(count < 3){
+    //         cout << data[count] << endl;
+    //         count++;
+    //     }
+    // }
 	
 	filein.close();
 	return 0;

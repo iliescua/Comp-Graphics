a6f44ccf5fac76fa3211f6556b861db281f0351f
Andy <iliescuandrew@gmail.com>
2020-03-12 14:38:28 -0500

    Cleaned up lab code and replaced printf's with cout's

--- Lab 1/lab1.cpp
+++ Lab 1/lab1.cpp
@@ -3,26 +3,28 @@
 #include<sstream>  //Read words from string 
 #include<iomanip>  //Format input/output
 #include<string>
-#include<stdio.h>
 using namespace std;
 
 int main(int argc, char* argv[]){
 	string filename = argv[1];
 	ifstream filein(filename.c_str());
-	string line;
-    string word;
-    string data;
-    int numFacets = 0;
-    float x=0,y=0,z=0;
-    float min_x=0,min_y=0,min_z=0;
-    float max_x=0,max_y=0,max_z=0;
-    float count=0,temp=0;
+	string line; //Line from file
+    string word; //A word in the line
+    string data; //Used as a temp variable for line
+    int numFacets = 0; //Keeps track of facets
+    float x=0,y=0,z=0; //Initializes the x,y,z coordinates
+    float min_x=0,min_y=0,min_z=0; //Initialized to store min coordinates
+    float max_x=0,max_y=0,max_z=0; //Initialized to store max coordinates
+    float count=0,temp=0; //Temp variabls used in calculations
+    int firstSet = 6; //First set of data size
 
-    while (count < 6){
+    //Iterates through first set of data
+    while (count < firstSet){
         getline(filein, line);
         istringstream ss(line);
         ss >> word;
         if(word == "vertex"){
+            //Loads in first set of data into the min and max values to be used later
             ss >> max_x;
             min_x = max_x;
             ss >> max_y;
@@ -30,8 +32,9 @@ int main(int argc, char* argv[]){
             ss >> max_z;
             min_z = max_z;
             numFacets++;
-            printf("\nFacet: %d\n", numFacets);
+            cout << "\nFacets: " << numFacets << endl;
             data = line;
+            //Prints out the first set of vertex values
             while(temp < 3){
                 cout << data << endl;
                 getline(filein, data);
@@ -41,12 +44,14 @@ int main(int argc, char* argv[]){
         temp = 0;
         count++;
     }
-    count = 0;
+    count = 0; //Resets count
 
+    //Iterates through the rest of the file
     while(getline(filein, line)){
         istringstream ss(line);
         ss >> word;
         if(word == "vertex"){
+            //Loads vertex values and compares them for min and max
             ss >> x;
             ss >> y;
             ss >> z;
@@ -70,7 +75,8 @@ int main(int argc, char* argv[]){
             }
             numFacets++;
             data = line;
-            printf("\nFacet: %d\n", numFacets);
+            cout << "\nFacets: " << numFacets << endl;
+            //Used to print out all three lines of vertex data in 1 facet
             while(count < 3){
                 cout << data << endl;
                 getline(filein, data);
@@ -79,13 +85,14 @@ int main(int argc, char* argv[]){
             count = 0;
         }
     }
-    printf("\n\nTotal Number of Facets is %d\n\n", numFacets);
-    printf("\nLargest x: %f", max_x);
-    printf("\nLargest y: %f", max_y);
-    printf("\nLargest z: %f\n", max_z);
-    printf("\nSmallest x: %f", min_x);
-    printf("\nSmallest y: %f", min_y);
-    printf("\nSmallest z: %f\n", min_z);
+    //Used to print out results at the end
+    cout << "\n\nTotal Number of Facets is " << numFacets << "\n\n" << endl;
+    cout << "Largest x: " << max_x << endl;
+    cout << "Largest y: " << max_y << endl;
+    cout << "Largest z: " << max_z << "\n" << endl;
+    cout << "Smallest x: " << min_x << endl;
+    cout << "Smallest y: " << min_y << endl;
+    cout << "Smallest z: " << min_z << endl;
 	
 	filein.close();
 	return 0;

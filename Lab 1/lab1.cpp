f69cce3b6a10b437f77ad0e4669aa5d551c48a70
Andy <iliescuandrew@gmail.com>
2020-03-12 10:56:09 -0500

    Lab work was finished and the code runs as expected. Comments need to be added and possible cleaning up can still be done

--- Lab 1/lab1.cpp
+++ Lab 1/lab1.cpp
@@ -3,6 +3,7 @@
 #include<sstream>  //Read words from string 
 #include<iomanip>  //Format input/output
 #include<string>
+#include<stdio.h>
 using namespace std;
 
 int main(int argc, char* argv[]){
@@ -10,11 +11,37 @@ int main(int argc, char* argv[]){
 	ifstream filein(filename.c_str());
 	string line;
     string word;
-    string data[] = {};
+    string data;
     int numFacets = 0;
-    int x,y,z = 0;
-    int min_x,min_y,min_z = 0;
-    int max_x,max_y,max_z = 0;
+    float x=0,y=0,z=0;
+    float min_x=0,min_y=0,min_z=0;
+    float max_x=0,max_y=0,max_z=0;
+    float count=0,temp=0;
+
+    while (count < 6){
+        getline(filein, line);
+        istringstream ss(line);
+        ss >> word;
+        if(word == "vertex"){
+            ss >> max_x;
+            min_x = max_x;
+            ss >> max_y;
+            min_y = max_y;
+            ss >> max_z;
+            min_z = max_z;
+            numFacets++;
+            printf("\nFacet: %d\n", numFacets);
+            data = line;
+            while(temp < 3){
+                cout << data << endl;
+                getline(filein, data);
+                temp++;
+            }
+        }
+        temp = 0;
+        count++;
+    }
+    count = 0;
 
     while(getline(filein, line)){
         istringstream ss(line);
@@ -41,19 +68,24 @@ int main(int argc, char* argv[]){
             if(z < min_z){
                 min_z = z;
             }
-            //data[numFacets] = line; //SEG FAULTS HERE
             numFacets++;
+            data = line;
+            printf("\nFacet: %d\n", numFacets);
+            while(count < 3){
+                cout << data << endl;
+                getline(filein, data);
+                count++;
+            }
+            count = 0;
         }
     }
-    numFacets /= 3;
-    int count = 0;
-    // for(int i = 0; i < numFacets; i++){
-    //     printf("Facet: %d       x   y   z", i);
-    //     while(count < 3){
-    //         cout << data[count] << endl;
-    //         count++;
-    //     }
-    // }
+    printf("\n\nTotal Number of Facets is %d\n\n", numFacets);
+    printf("\nLargest x: %f", max_x);
+    printf("\nLargest y: %f", max_y);
+    printf("\nLargest z: %f\n", max_z);
+    printf("\nSmallest x: %f", min_x);
+    printf("\nSmallest y: %f", min_y);
+    printf("\nSmallest z: %f\n", min_z);
 	
 	filein.close();
 	return 0;
